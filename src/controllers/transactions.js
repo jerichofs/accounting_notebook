9c8d01a9a8cd6ed49182777302bc6c280d90a209
jerichofs <kiberonex@protonmail.com>
2020-01-07 18:59:08 +0200

    Server: add transaction endpoint

--- src/controllers/transactions.js
+++ src/controllers/transactions.js
@@ -1,3 +1,4 @@
+const uuidv4 = require("uuid/v4");
 let data = require("../data");
 
 const getTransactions = (req, res) => {
@@ -20,7 +21,46 @@ const getTransactionById = (req, res) => {
   return res.status(200).json(transaction);
 };
 
+const addTransaction = (req, res) => {
+  console.log("body ", req.body);
+  const { type, amount } = req.body;
+  let transaction;
+
+  if (type === "credit") {
+    data.accountValue += +amount;
+
+    transaction = {
+      id: uuidv4(),
+      type,
+      amount: data.accountValue,
+      effectiveDate: new Date()
+    };
+
+    data.transactions.push(transaction);
+  }
+
+  if (type === "debit") {
+    if (data.accountValue - +amount < 0) {
+      return res.status(403).json("Account does not have enough money");
+    }
+
+    data.accountValue -= +amount;
+
+    transaction = {
+      id: uuidv4(),
+      type,
+      amount: data.accountValue,
+      effectiveDate: new Date()
+    };
+
+    data.transactions.push(transaction);
+  }
+
+  return res.status(200).json(transaction);
+};
+
 module.exports = {
   getTransactions,
-  getTransactionById
+  getTransactionById,
+  addTransaction
 };
--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const transactions = express.Router();
+const bodyParser = require("body-parser");
 
 const {
   getTransactions,
-  getTransactionById
+  getTransactionById,
+  addTransaction
 } = require("../controllers/transactions");
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 // Fetch transactions history
 transactions.get("/", getTransactions);
 
 // Find transaction by ID
 transactions.get("/:transactionId", getTransactionById);
+
+// Add new transaction
+transactions.post("/", urlencodedParser, addTransaction);
 
 // handle 404 error
 transactions.use((req, res) => {
