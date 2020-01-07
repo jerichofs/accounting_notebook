b09a0315167dc20465500c096a7762d7e4fc29f3
jerichofs <kiberonex@protonmail.com>
2020-01-07 18:39:49 +0200

    Server: get transaction by id endpoint

--- src/controllers/transactions.js
+++ src/controllers/transactions.js
@@ -6,6 +6,21 @@ const getTransactions = (req, res) => {
   });
 };
 
+const getTransactionById = (req, res) => {
+  const { transactionId } = req.params;
+
+  const transaction = data.transactions.find(
+    element => element.id === transactionId
+  );
+
+  if (!transaction) {
+    return res.status(404).json("transaction not found");
+  }
+
+  return res.status(200).json(transaction);
+};
+
 module.exports = {
-  getTransactions
+  getTransactions,
+  getTransactionById
 };
--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const transactions = express.Router();
 
-const { getTransactions } = require("../controllers/transactions");
+const {
+  getTransactions,
+  getTransactionById
+} = require("../controllers/transactions");
 
 // Fetch transactions history
 transactions.get("/", getTransactions);
+
+// Find transaction by ID
+transactions.get("/:transactionId", getTransactionById);
 
 // handle 404 error
 transactions.use((req, res) => {
